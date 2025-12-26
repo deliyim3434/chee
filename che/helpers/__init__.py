4628b7afc8c120b01225b20554d5a023d54ccf3c
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-12-26 20:55:55 +0300

    Update __init__.py

--- che/helpers/__init__.py
+++ che/helpers/__init__.py
@@ -6,9 +6,9 @@ from ._dataclass import Media, Track
 from ._exec import format_exception, meval
 from ._inline import Inline
 from ._queue import Queue
-from ._thumbnails import Thumbnail
+
 from ._utilities import Utilities
 
 buttons = Inline()
-thumb = Thumbnail()
+
 utils = Utilities()
